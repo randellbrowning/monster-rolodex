8584113457dc61b29ee65cdfde42ee47a055421b
randell browning <randell.browning@gmail.com>
2019-11-21 23:31:48 -0500

    Updates

---
+++ precache-manifest.fbf3d8cd5116ec9d03b2bdac979eec98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0347d1c5ac42fe4505dfffb588e708",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9612cf85d9d320c460af",
+    "url": "/monster-rolodex/static/css/main.dffe5980.chunk.css"
+  },
+  {
+    "revision": "a16792bbd021bec6ef7e",
+    "url": "/monster-rolodex/static/js/2.27899654.chunk.js"
+  },
+  {
+    "revision": "9612cf85d9d320c460af",
+    "url": "/monster-rolodex/static/js/main.e6a40e47.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
